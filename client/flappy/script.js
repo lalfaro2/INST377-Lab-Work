ad904251c3067cb99f3ef47f79bc31ef1a0b1e7b
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-18 11:19:18 -0400

    bird falling

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -5,9 +5,12 @@ document.addEventListener("DOMContentLoaded" , () => {
 
     let birdLeft = 220
     let birdBottom = 100
+    let gravity = 2
 
     function startGame() {
+        birdBottom -= gravity
         bird.style.bottom = birdBottom + "px"
+        bird.style.left = birdLeft + "px"
     }
-    startGame()
+    let timerId = setInterval(startGame, 20)
 })
