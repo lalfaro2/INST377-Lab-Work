2c9adc9fd65e512c50f12f9ff7ee0197e80af3ed
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-18 12:33:24 -0400

    floor bird

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -6,13 +6,14 @@ document.addEventListener("DOMContentLoaded" , () => {
     let birdLeft = 220
     let birdBottom = 100
     let gravity = 2
+    let isGameOver = false
 
     function startGame() {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + "px"
         bird.style.left = birdLeft + "px"
     }
-    let timerId = setInterval(startGame, 20)
+    let gameTimerId = setInterval(startGame, 20)
 
     function control(e){
         if (e.keyCode === 32) {
@@ -43,9 +44,18 @@ document.addEventListener("DOMContentLoaded" , () => {
                 clearInterval(timerId)
                 display.removeChild(obstacle)
             }
+            if (birdBottom === 0){
+                gameOver()
+            }
         }
         let timerId = setInterval(moveObstacle, 20)
         setTimeout(generateObstacle, 3000)
     }
     generateObstacle()
+
+    function gameOver() {
+        clearInterval(gameTimerId)
+        isGameOver = true
+        document.removeEventListener("keyup", control)
+    }
 })
