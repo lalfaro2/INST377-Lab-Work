aa5a7b98a1f5249d5e74692f2536f3503bf979ad
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-22 10:03:03 -0400

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,2 +1,5 @@
 let slidePosition = 0;
-const pics = document.querySelectorAll(".carousel_item")
+const pics = document.querySelectorAll(".carousel_item");
+const totalPics = pics.length;
+
+console.log(totalPics);
