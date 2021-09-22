7fc22f4abb70b5db321f8aa6571be71a06cc9ee1
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-22 11:43:44 -0400

    added moving images

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,4 +2,37 @@ let slidePosition = 0;
 const pics = document.querySelectorAll(".carousel_item");
 const totalPics = pics.length;
 
-console.log(totalPics);
+
+document.querySelector('#next-button').addEventListener('click', function() {
+    moveToNext();
+})
+
+document.querySelector('#prev-button').addEventListener('click', function() {
+    moveToPrev();
+})
+
+function updateSlidePosition() {
+    for (let pic of pics) {
+        pic.classList.remove('first-item');
+        pic.classList.add('carousel_item--hidden');
+    }
+    pics[slidePosition].classList.add('first-item');
+}
+
+function moveToNext() {
+    if (slidePosition === totalPics - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition();
+}
+
+function moveToPrev() {
+    if (slidePosition === 0) {
+        slidePosition = totalPics - 1;
+    } else {
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
