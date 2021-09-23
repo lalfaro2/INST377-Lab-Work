e51a770e88e451a42eac3d099b05243f44a86c4b
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-22 21:17:42 -0400

    jumping platform and restarting point

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -2,15 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let startPoint = 150
+    let doodlerBottomSpace = startPoint
     let isGameOver = false
     let platforms = []
     let upTimerId
     let downTimerId
+    let isJumping = true
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
@@ -50,10 +53,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function jump () {
         clearInterval(downTimerId)
+        isJumping = true
         upTimerId = setInterval(function () {
             doodlerBottomSpace +=20
             doodler.style.bottom = doodlerBottomSpace + 'px'
-            if (doodlerBottomSpace > 350) {
+            if (doodlerBottomSpace > startPoint + 200) {
                 fall()
             }
         }, 30)
@@ -61,16 +65,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function fall () {
         clearInterval(upTimerId)
+        isJumping = false
         downTimerId = setInterval(function (){
             doodlerBottomSpace -= 5
             doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0) {
+                gameOver()
+            }
+            platforms.forEach(platform => {
+                if (
+                    (doodlerBottomSpace >= platform.bottom) &&
+                    (doodlerBottomSpace <= platform.bottom + 15) &&
+                    ((doodlerLeftSpace + 60) >= platform.left) &&
+                    (doodlerLeftSpace <= (platform.left + 85)) &&
+                    !isJumping
+                ) {
+                    startPoint = doodlerBottomSpace
+                    jump()
+                }
+            })
         }, 30)
+    }
+
+    function gameOver() {
+        isGameOver = true
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
+    }
+
+    function control (e) {
+        if (e.key === 'ArrowLeft') {
+            //move l
+        } else if (e.key === 'ArrowRight') {
+            //move r
+        } else if (e.key === 'ArrowUp') {
+            //move straight
+        }
     }
 
     function start() {
         if (!isGameOver) {
-            createDoodler()
             createPlatforms()
+            createDoodler()
             setInterval (movePlatforms, 30)
             jump()
         }
