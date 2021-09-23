b5782beed8796692af246ccaf2f2d021837767b9
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-22 20:11:50 -0400

    created random platforms and fixed isGameOver

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
     let doodlerBottomSpace = 150
+    let isGameOver = false
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -10,5 +11,35 @@ document.addEventListener('DOMContentLoaded', () => {
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
-    createDoodler()
+
+    class Platform {
+        constructor(newPlatBottom) {
+            this.bottom = newPlatBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+            const visual = this.visual
+            visual.classList.add('platform')
+
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms() {
+        for (let i = 0; i < 5; i++) {
+            let platformGap = 600 / 5
+            let newPlatBottom = 100 + i * platformGap
+            let newPlatform = new Platform(newPlatBottom)
+        }
+    }
+
+    function start() {
+        if (!isGameOver) {
+            createDoodler()
+            createPlatforms()
+        }
+    }
+
+    start()
 })
