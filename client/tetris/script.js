a494ba4d51fd084f9178ea596efa9ebdf1a7d4a2
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-30 15:03:20 -0400

    fixed eslint problems and added variables

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,5 +1,7 @@
-const grid = document.querySelector('.grid');
-let squares = Array.from(document.querySelector('.grid div'));
-const width = 10;
-
-console.log(squares);
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  const squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('start-button')
+});
