57a475214399ea8614b97566886657554573e1bb
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-09-30 12:05:31 -0400

    fixing js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-})
+const grid = document.querySelector('.grid');
+let squares = Array.from(document.querySelector('.grid div'));
+const width = 10;
+
+console.log(squares);
