4034fe26c63cd5e014cabbba9d6dbb35321a7727
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-10-06 09:47:35 -0400

    extra instructions completed

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,33 +1,37 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const restaurant = [];
+async function windowActions() {
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-fetch(endpoint).then((blob) => blob.json()).then((data) => restaurant.push(...data));
+    const request = await fetch(endpoint);
+    const restaurant = await request.json();
 
-function findMatches(word, restaurant) {
-  return restaurant.filter(food => {
+    function findMatches(word, restaurant) {
+    return restaurant.filter(food => {
 
-    const regex = new RegExp(word, 'gi');
-    return food.zip.match(regex);
-  });
-}
+        const regex = new RegExp(word, 'gi');
+        return food.zip.match(regex);
+    });
+    }
 
-function displayMatches() {
-  const matchArray = findMatches(this.value, restaurant);
-  const html = matchArray.map(food => {
-      return `
-      <li>
-        <span class = 'name'>${food.name}<br></span>
-        <span class = 'address1'>${food.address_line_1}<br></span>
-        <span class = 'citystate'>${food.city}, ${food.state}<br></span>
-        <span class = 'zipcode'>${food.zip}</span>
-      </li><br>
-      `;
-  }).join('');
-  result.innerHTML = html;
-}
+    function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, restaurant);
+    const html = matchArray.map(food => {
+        return `
+        <li>
+            <span class = 'name'>${food.name}<br></span>
+            <em><span class = 'address1'>${food.address_line_1}<br></span>
+            <span class = 'citystate'>${food.city}, ${food.state}<br></span>
+            <span class = 'zipcode'>${food.zip}</span></em>
+        </li><br>
+        `;
+    }).join('');
+    result.innerHTML = html;
+    }
 
-const searchInput = document.querySelector('.search');
-const result = document.querySelector('.results');
+    const searchInput = document.querySelector('.search');
+    const result = document.querySelector('.results');
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+    searchInput.addEventListener('change', displayMatches);
+    searchInput.addEventListener('keyup', (evt) => {displayMatches(evt)});
+};
+
+window.onload = windowActions();
