908f7c8b60870869f03db7be9d75357511db0f16
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-10-13 19:43:43 -0400

    markers and panning

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,5 +1,5 @@
 function mapInit() {
-  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  const mymap = L.map('mapid').setView([38.98582939, -76.937329584], 14);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -9,12 +9,10 @@ function mapInit() {
     accessToken: 'pk.eyJ1Ijoibm51a2t5IiwiYSI6ImNrdXBtYXJ1NjRtazEycW56MzlmZmMxMmIifQ.CpxpF7AWl9rX_5fsi4mrJA'
   }).addTo(mymap);
 
-  var marker = L.marker([51.5, -0.09]).addTo(mymap);
+  return mymap;
 }
 
-mapInit();
-
-async function dataHandler() {
+async function dataHandler(map) {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
   const request = await fetch(endpoint);
@@ -28,11 +26,19 @@ async function dataHandler() {
   }
 
   function displayMatches(event) {
+    const markers = L.layerGroup().addTo(map);
+    markers.clearLayers();
     let matchArray = [];
     matchArray = findMatches(event.target.value, restaurant);
     matchArray = matchArray.slice(0, 5);
     let locationArr = matchArray.map(item => item.geocoded_column_1.coordinates);
     locationArr = locationArr.map(item => item.reverse());
+    map.panTo([locationArr[0][0], locationArr[0][1]]);
+    L.marker([locationArr[0][0], locationArr[0][1]]).addTo(markers);
+    L.marker([locationArr[1][0], locationArr[1][1]]).addTo(markers);
+    L.marker([locationArr[2][0], locationArr[2][1]]).addTo(markers);
+    L.marker([locationArr[3][0], locationArr[3][1]]).addTo(markers);
+    L.marker([locationArr[4][0], locationArr[4][1]]).addTo(markers);
     const html = matchArray.map(food => `
         <div class = finalResult>
             <span class = 'name'><strong>${food.name}</strong><br></span>
@@ -50,4 +56,9 @@ async function dataHandler() {
   searchInput.addEventListener('submit', (evt) => {displayMatches(evt)});
 }
 
-window.onload = dataHandler();
+function windowActions() {
+  const myMap = mapInit();
+  dataHandler(myMap);
+}
+
+window.onload = windowActions;
