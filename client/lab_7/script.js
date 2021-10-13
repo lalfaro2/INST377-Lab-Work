278859e1605dc1bbfec39e0a4b79ffbdd1f2b97f
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-10-13 08:45:49 -0400

    added map

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,4 +1,18 @@
-async function windowActions() {
+function mapInit() {
+  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+  }).addTo(mymap);
+}
+
+mapInit();
+
+async function dataHandler() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
   const request = await fetch(endpoint);
@@ -29,6 +43,6 @@ async function windowActions() {
 
   searchInput.addEventListener('change', displayMatches);
   searchInput.addEventListener('keyup', (evt) => {displayMatches(evt)});
-};
+}
 
-window.onload = windowActions();
+window.onload = dataHandler();
