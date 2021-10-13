47644f2286f9ac4f640f6b0b94d72d900a15371b
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-10-13 10:51:33 -0400

    working map

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,12 +1,12 @@
 function mapInit() {
-  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1Ijoibm51a2t5IiwiYSI6ImNrdXBtYXJ1NjRtazEycW56MzlmZmMxMmIifQ.CpxpF7AWl9rX_5fsi4mrJA'
   }).addTo(mymap);
 }
 
@@ -42,7 +42,7 @@ async function dataHandler() {
   const result = document.querySelector('.results');
 
   searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
+  searchInput.addEventListener('submit', (evt) => {displayMatches(evt)});
 }
 
 window.onload = dataHandler();
